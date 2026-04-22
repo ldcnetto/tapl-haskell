cd9d1e5c9a22225e48da5ca47d02883e280638d4
rbonifacio <rbonifacio123@gmail.com>
2026-04-22 18:33:22 -0300

    type checker using Either

--- lib/TypeChecker.hs
+++ lib/TypeChecker.hs
@@ -1,6 +1,31 @@
 module TypeChecker where
 
 import AST
+import Prelude hiding (fail)
 
-checker :: Expr -> Type
-checker _expr = undefined
+type MErr = Either String Type
+
+fail :: String -> MErr
+fail = Left
+
+checker :: Expr -> MErr
+checker expr = case expr of
+  ETrue -> return TBool
+  EFalse -> return TBool
+  If c t e -> do
+    tc <- checker c
+    tt <- checker t
+    te <- checker e
+    if tc == TBool
+      then if tt == te then return tt else fail "both then and else clauses must have the same type"
+      else fail "expecting boolean type in condition."
+  Zero -> return TNat
+  Succ e -> do
+    te <- checker e
+    if te == TNat then return TNat else fail "Succ expects a TNat"
+  Pred e -> do
+    te <- checker e
+    if te == TNat then return TNat else fail "Pred expects a TNat"
+  IsZero e -> do
+    te <- checker e
+    if te == TNat then return TBool else fail "IsZero expects a TNat"
