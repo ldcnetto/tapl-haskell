6f1ade56750edba33210d75fabe669e617c4272b
Leonidas <leonidas.castro+bradesco@bradesco.com.br>
2026-05-17 21:42:38 -0300

    feat: implementando pairs e records

--- lib/AST.hs
+++ lib/AST.hs
@@ -12,6 +12,11 @@ data Expr = ETrue
           | Var Name                  -- x               vars in Lambda Calculus
           | Abs (Name, Type) Expr     -- (\x:T . expr)   abstraction in Lambda Calculus
           | App Expr Expr             -- t1 t2           application in Lambda Calculus
+          | Pair Expr Expr            -- {t1, t2}        pair creation
+          | Fst Expr                  -- t.1             first projection
+          | Snd Expr                  -- t.2             second projection
+          | Record [(Name, Expr)]     -- {l1=t1, ...}    record creation
+          | Proj Expr Name            -- t.l             record projection
      deriving (Eq, Show)
 
 data Value = VTrue
@@ -19,9 +24,13 @@ data Value = VTrue
            | VZero
            | VSucc Value
            | VAbs (Name, Type) Expr
+           | VPair Value Value
+           | VRecord [(Name, Value)]
      deriving (Eq, Show)
 
 data Type = TBool
           | TNat
           | Type `TArrow` Type
+          | TProd Type Type           -- T1 x T2         product type
+          | TRecord [(Name, Type)]    -- {l1:T1, ...}    record type
      deriving (Eq, Show)
